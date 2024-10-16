920b865e9e3c85d316fc1956b840be6019d7dcd0
Hamidullo-Ahmedov <hamidulloahmedov1202@gmail.com>
2024-10-16 22:23:41 +0500

    Cover the header part

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-// Write your JS codes here
+var elSiteHeader = document.querySelector(".js-header")
+var elMenuBtn = elSiteHeader.querySelector(".js-hamburger-btn")
+var elCancelBtn = elSiteHeader.querySelector(".js-cancel-btn")
+
+elMenuBtn.addEventListener("click", function(){
+
+  console.log("Hamburger btn bosildi");
+
+  elSiteHeader.classList.add("show-sidebar");
+  
+  
+});
+
+elCancelBtn.addEventListener("click", function(){
+
+  console.log("Cancel btn bosildi");
+
+  elSiteHeader.classList.remove("show-sidebar");
+  
+  
+});
